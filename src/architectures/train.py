f6af516efe173f60243c5f725df0dbdef5e19c41
francescoopiccoli <xfrancescopiccolix@gmail.com>
2023-09-21 15:32:45 +0200

    feat: add multigraph training functions

--- src/architectures/train.py
+++ src/architectures/train.py
@@ -1,10 +1,13 @@
 import torch
-from utils import train, evaluate, test
+from utils import train_single_graph, evaluate_single_graph, test_single_graph, train_epoch, evaluate_epoch, test_epoch
 from GAT import GAT
 from torch_geometric.datasets import Planetoid
+from torch_geometric.datasets import PPI
 from torch_geometric.transforms import NormalizeFeatures
+from torch_geometric.data import DataLoader
 
-# import data
+"""# single graph training
+# dataset contains 1 graph
 dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
 data = dataset[0]
 
@@ -20,9 +23,43 @@ criterion = torch.nn.CrossEntropyLoss()
 
 # train
 for epoch in range(1, 150):
-    loss = train(data=data, model=model, optimizer=optimizer, criterion=criterion)
-    val_loss = evaluate(data=data, model=model, criterion=criterion)
-    test_acc = test(data=data, model=model)
+    loss = train_single_graph(data=data, model=model, optimizer=optimizer, criterion=criterion)
+    val_loss = evaluate_single_graph(data=data, model=model, criterion=criterion)
+    test_acc = test_single_graph(data=data, model=model)
 
     if epoch % 10 == 0:
-        print(f'Epoch: {epoch:03d}, trn_Loss: {loss:.3f}, val_loss: {val_loss:.3f}, tst_acc: {test_acc:.3f}')
+        print(f'Epoch: {epoch:03d}, trn_Loss: {loss:.3f}, val_loss: {val_loss:.3f}, tst_acc: {test_acc:.3f}')"""
+
+#############################################################################################################
+
+# multi graph training
+
+# use ppi dataset
+"""train_dataset = ...
+val_dataset = ...
+test_dataset = ...
+
+
+# instantiate model
+model = GAT(dim_input=train_dataset.num_features, dim_hidden=[5, 3], dim_output=train_dataset.num_classes, num_layers=2, heads=[1,1])
+
+# setup training
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
+criterion = torch.nn.CrossEntropyLoss()
+
+# setup dataloaders for trian, val and test
+train_data_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+val_data_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
+test_data_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
+
+# train
+for epoch in range(1, 5):
+    loss = train_epoch(model=model, loader=train_data_loader, optimizer=optimizer, criterion=criterion)
+    val_loss = evaluate_epoch(model=model, loader=val_data_loader, criterion=criterion)
+    test_acc = test_epoch(model=model, loader=test_data_loader)
+    
+    print(f'Epoch: {epoch:03d}, trn_Loss: {loss:.3f}, val_loss: {val_loss:.3f}, tst_acc: {test_acc:.3f}')"""
+
+
+
+
--- src/architectures/utils.py
+++ src/architectures/utils.py
@@ -1,4 +1,5 @@
-def train(data, model, optimizer, criterion, device='cpu'):
+# Single graph training functions
+def train_single_graph(data, model, optimizer, criterion, device='cpu'):
     model.train()
     model.to(device)
     optimizer.zero_grad()
@@ -8,14 +9,14 @@ def train(data, model, optimizer, criterion, device='cpu'):
     optimizer.step()
     return loss
 
-def evaluate(data, model, criterion, device='cpu'):
+def evaluate_single_graph(data, model, criterion, device='cpu'):
     model.eval()
     model.to(device)
     out = model(data)
     loss = criterion(out[data.val_mask], data.y[data.val_mask])
     return loss
 
-def test(data, model, device='cpu'):
+def test_single_graph(data, model, device='cpu'):
     model.eval()
     model.to(device)
     out = model(data)
@@ -23,3 +24,57 @@ def test(data, model, device='cpu'):
     correct = pred[data.test_mask] == data.y[data.test_mask]
     acc = int(correct.sum()) / int(data.test_mask.sum())  # Derive ratio of correct predictions.
     return acc
+
+#############################################################################################################
+# Multi graph training functions
+
+def train_epoch(model, loader, optimizer, criterion, device='cpu'):
+  model.train()
+  model.to(device)
+  optimizer.zero_grad()
+
+  total_loss = 0.0
+
+  for batch in loader:
+    batch = batch.to(device)
+    optimizer.zero_grad()
+    output = model(batch)
+    loss = criterion(output, batch.y.long())
+    loss.backward()
+    optimizer.step()
+    total_loss += loss.item()
+
+  return total_loss / len(loader)
+
+
+def evaluate_epoch(model, loader, criterion, device='cpu'):
+    model.eval()
+    model.to(device)
+
+    total_loss = 0.0
+    for batch in loader:
+        batch = batch.to(device)
+        output = model(batch)
+        loss = criterion(output, batch.y.long())
+        total_loss += loss.item()
+
+    return total_loss / len(loader)
+
+
+def test_epoch(model, loader, device='cpu'):
+    model.eval()
+    model.to(device)
+
+    correct = 0
+    total_samples = 0
+    
+    for batch in loader:
+        batch = batch.to(device)
+        output = model(batch)
+        pred = output.argmax(dim=1)
+        correct += int((pred == batch.y).sum())
+        total_samples += batch.num_graphs * batch.num_nodes
+
+    accuracy = correct / total_samples
+
+    return accuracy
