030dbf3b39a8fff1241b76d2a9994f7a2e1e81a4
francescoopiccoli <xfrancescopiccolix@gmail.com>
2023-09-26 10:37:11 +0200

    feat: implement random walk subgraphing

--- Data/subgraphs_methods.py
+++ Data/subgraphs_methods.py
@@ -51,3 +51,53 @@ def bfs_neighbor_selection(full_net, starting_bus, subgraph_length):
     return visited
 
 
+def random_walk_neighbor_selection(full_net, starting_bus, subgraph_length):
+    restart_prob=0.2
+    previous_step_bus = starting_bus
+    current_bus = starting_bus
+    subgraph_busses = [starting_bus]
+    stuck_iteration = 0
+
+    while len(subgraph_busses) < subgraph_length:
+        # Check if we should restart the random walk with a certain probability
+        if np.random.rand() < restart_prob:
+            current_bus = starting_bus
+
+        # Get all busses directly connected to the current bus (from/to)
+        f = full_net.line.to_bus[np.where(np.array(full_net.line.from_bus) == current_bus)[0]]
+        t = full_net.line.from_bus[np.where(np.array(full_net.line.to_bus) == current_bus)[0]]
+
+        # Get all busses connected to the current bus via a transformer (trafo) either high voltage (hv) or low voltage (lv)
+        f_trafo = full_net.trafo.hv_bus[np.where(np.array(full_net.trafo.lv_bus) == current_bus)[0]]
+        t_trafo = full_net.trafo.lv_bus[np.where(np.array(full_net.trafo.hv_bus) == current_bus)[0]]
+
+        connected = np.concatenate((f, t, f_trafo, t_trafo))
+
+        # Remove all busses that are already in the subgraph
+        new_busses = np.setdiff1d(connected, subgraph_busses)
+
+        
+        if len(new_busses) == 0:
+            stuck_iteration += 1
+            if stuck_iteration > 20:
+                # pick a random bus from the subgraph
+                # random walk with random backtracking to avoid getting stuck in dead ends
+                current_bus = subgraph_busses[np.random.randint(0, len(subgraph_busses))]
+                stuck_iteration = 0
+                continue
+            else:
+                # pick the previous bus
+                current_bus = previous_step_bus
+                continue
+
+        # Pick a random bus from the remaining busses in new_busses
+        next_bus = new_busses[np.random.randint(0, len(new_busses))]
+
+        # Move to the next bus in the random walk
+        previous_step_bus = current_bus
+        current_bus = next_bus
+        subgraph_busses.append(current_bus)
+
+    return subgraph_busses
+
+# other methods to try: k-hop neighborhood, Community Detection, random walk laplacian,graphSAINT partitioning, ...
