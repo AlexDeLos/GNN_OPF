c7092b42ac9ca431d49e4a42ce7bff95f816ad3a
AlexDeLos <alexdelos2001@gmail.com>
2023-10-06 13:37:45 +0200

    start

--- src/train.py
+++ src/train.py
@@ -1,5 +1,8 @@
 from torch_geometric.loader import DataLoader as pyg_DataLoader
 import tqdm
+from queue import Queue
+import torch as th
+import utils
 from utils import get_gnn, get_optim, get_criterion
 
 
@@ -61,11 +64,42 @@ def train_batch(data, model, optimizer, criterion, device='cpu'):
     model.to(device)
     optimizer.zero_grad()
     out = model(data)
+    distance = get_distance_from_generator(data)
     loss = criterion(out, data.y)
     loss.backward()
     optimizer.step()
     return loss
 
+def get_distance_from_generator(data):
+    for node in data.x:
+        #if the p_mw_gen is > 0 then it is a generator
+        p_mw_gen = node[1]
+        if p_mw_gen > 0:
+            bfs(data,node)
+        
+    distance = th.zeros(data.x.shape[0], 1)
+    return distance
+
+def get_neighbors(data, node):
+    neighbors = []
+    edges = data.edge_index
+    print(edges)
+
+    return neighbors
+
+
+def bfs(graph, source):
+    Q = Queue()
+    visited_vertices = set()
+    Q.put(source)
+    visited_vertices.update({0})
+    while not Q.empty():
+        vertex = Q.get()
+        print(vertex, end="-->")
+        for u in get_neighbors(graph, vertex):
+            if u not in visited_vertices:
+                Q.put(u)
+                visited_vertices.update({u})
 
 def evaluate_batch(data, model, criterion, device='cpu'):
     model.to(device)
