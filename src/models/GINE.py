8bd7b2e51e5763d6f7848f912e4c6ca04582591b
AlexDeLos <alexdelos2001@gmail.com>
2023-10-15 11:34:34 +0200

    Updated GINE

--- src/models/GINE.py
+++ src/models/GINE.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Linear, Sequential, BatchNorm1d, ReLU, Dropout
 from torch_geometric.nn import GINEConv
@@ -9,17 +10,20 @@ class GINE(torch.nn.Module):
 
     class_name = "GINE"
     
-    def __init__(self, input_dim, output_dim, edge_dim, hidden_gine_dim=16, hidden_lin_dim=64, dropout_rate=0.5, jumping_knowledge=False):
+    def __init__(self, input_dim, output_dim, edge_dim, hidden_gine_dim=16, hidden_lin_dim=64, dropout_rate=0.5, jumping_knowledge=False, n_of_convs_in=1):
         super(GINE, self).__init__()
         self.conv1 = GINEConv(
             Sequential(Linear(input_dim, hidden_gine_dim),
                        BatchNorm1d(hidden_gine_dim), ReLU(),
                        Linear(hidden_gine_dim, hidden_gine_dim), ReLU()),
                        edge_dim=edge_dim) #[1]
-        self.conv2 = GINEConv(
-            Sequential(Linear(hidden_gine_dim, hidden_gine_dim), BatchNorm1d(hidden_gine_dim), ReLU(),
-                       Linear(hidden_gine_dim, hidden_gine_dim), ReLU()),
-                       edge_dim=edge_dim) #[1]
+        
+        self.convs2 = nn.ModuleList()
+        for _ in range(n_of_convs_in):
+            self.convs2.append(GINEConv(
+                Sequential(Linear(hidden_gine_dim, hidden_gine_dim), BatchNorm1d(hidden_gine_dim), ReLU(),
+                        Linear(hidden_gine_dim, hidden_gine_dim), ReLU()),
+                        edge_dim=edge_dim)) #[1]
         self.conv3 = GINEConv(
             Sequential(Linear(hidden_gine_dim, hidden_gine_dim), BatchNorm1d(hidden_gine_dim), ReLU(),
                        Linear(hidden_gine_dim, hidden_gine_dim), ReLU()),
@@ -27,7 +31,7 @@ class GINE(torch.nn.Module):
 
         # https://pytorch-geometric.readthedocs.io/en/latest/generated/torch_geometric.nn.models.JumpingKnowledge.html
         if jumping_knowledge:
-            n_of_convs = 3
+            n_of_convs = n_of_convs_in + 2
             # if setting mode to 'cat', change the channels of lin1 to hidden_gine_dim * n_of_convs
             self.jumping_knowledge = JumpingKnowledge(mode='lstm', channels=hidden_gine_dim, num_layers=n_of_convs)
         self.lin1 = Linear(hidden_gine_dim, hidden_lin_dim)
@@ -43,9 +47,13 @@ class GINE(torch.nn.Module):
         # Node embeddings 
         h1 = self.conv1(x, edge_index=edge_index, edge_attr=edge_attr)
         xs.append(h1)
-        h2 = self.conv2(h1, edge_index=edge_index, edge_attr=edge_attr)
-        xs.append(h2)
-        h3 = self.conv3(h2, edge_index=edge_index, edge_attr=edge_attr)
+        x = h1
+        for c in self.convs2:
+            x = c(x, edge_index=edge_index, edge_attr=edge_attr)
+            xs.append(x)
+        # h2 = self.convs2(h1, edge_index=edge_index, edge_attr=edge_attr)
+        xs.append(x)
+        h3 = self.conv3(x, edge_index=edge_index, edge_attr=edge_attr)
         xs.append(h3)
 
         if hasattr(self, 'jumping_knowledge'):
