6b8ba2bdfe269ff6b1ed3aec71c47ead9f1c3143
AlexDeLos <alexdelos2001@gmail.com>
2023-11-01 15:59:31 +0100

    added updates

--- src/models/plot_graph.py
+++ src/models/plot_graph.py
@@ -8,10 +8,21 @@ from utils.utils_plot import distance_plot, plot_losses, plot_percent_curve
 def main():
     csv_dict = {
         'physics_loss_-0' : 'Data/results/physics_0_results.csv',
-        'physics_loss_-1' : 'Data/results/-1(test).csv',
-        'physics_loss_-5' : 'Data/results/physics_5_results.csv',
+        'physics_loss_-1' : 'Data/results/physics_1_results.csv',
+        'physics_loss_-2' : 'Data/results/physics_2_results.csv',
+        'physics_loss_-3' : 'Data/results/physics_3_results.csv',
+        'physics_loss_-10' : 'Data/results/physics_10_results.csv',
+        'physics_loss_-15' : 'Data/results/physics_15_results.csv',
+        'physics_loss_-20' : 'Data/results/physics_20_results.csv',
     }
 
-    plot_percent_curve(csv_dict, col_name= 'load_vm_pu')
+    # load_vm_pu
+    # load_va_deg
+    # gen_va_deg
+    # load_gen_va_deg
+    plot_percent_curve(csv_dict, col_name= 'load_vm_pu', colors=['red', 'blue', 'green', 'orange', 'purple', 'black', 'grey'])
+    plot_percent_curve(csv_dict, col_name= 'load_va_deg', colors=['red', 'blue', 'green', 'orange', 'purple', 'black', 'grey'])
+    plot_percent_curve(csv_dict, col_name= 'gen_va_deg', colors=['red', 'blue', 'green', 'orange', 'purple', 'black', 'grey'])
+    plot_percent_curve(csv_dict, col_name= 'load_gen_va_deg', colors=['red', 'blue', 'green', 'orange', 'purple', 'black', 'grey'])
 
 main()
